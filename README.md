87c7c78ad1a0a68f863e5bfeef7c9ffaceeb3b8d
piyusha22 <gpiyusha19@tbc.edu.np>
2023-03-12 23:19:35 +0545

    Modified the comments and README.md file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# Developed by Piyusha Gurung, 77261101
+
+## Unit Converter Application
+
+It is a unit application for Android where user can convert values between different units of measurement such as milliliters to fluid ounces and grams to cups and also can choose to perform reverse conversion of the units.
+
+## Key Features of Unit Converter Application :
+
+1. Numeric Value Input
+2. Unit Conversion Options
+3. Conversion Calculation
+4. Reverse Calculation
+5. Result Display
+6. Error Handling
+
 ![](UnitConverter.gif)
--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -11,13 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** saves the reference to the Button, EditText, RadioGroup, TextView and Switch
+         * object in a variable called unitBtn, unitNum, unitOptions, unitResult and unitReverse
+         * respectively and finds them in the layout with its unique resource ID
+         */
         val unitBtn : Button = findViewById<Button>(R.id.convert_button)
         val unitNum : EditText = findViewById<EditText>(R.id.unit)
         val unitOptions : RadioGroup = findViewById<RadioGroup>(R.id.unit_options)
         val unitResult : TextView = findViewById<TextView>(R.id.result)
         val unitReverse : Switch = findViewById<Switch>(R.id.reverse_switch)
 
+        //set a click listener on unitBtn object by calling the setOnClickListener() method
         unitBtn.setOnClickListener(View.OnClickListener {
+
+            /** retrieves the selected unit conversion option from the unitOptions radio group.
+             * Based on the selected option and the state of the unitReverse switch, it performs the
+             * corresponding unit conversion calculation using the value entered in the unitNum field.
+             * The calculated result is displayed in the unitResult text view. If a NumberFormatException
+             * occurs during the conversion, an error message is logged. */
             var converted = 0f
             val unitOptionsId: Int = unitOptions.checkedRadioButtonId
             try {
