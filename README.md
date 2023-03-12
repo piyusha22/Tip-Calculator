e884968174fcd5da237d2bd406840031203ddb52
piyusha22 <gpiyusha19@tbc.edu.np>
2023-03-12 23:00:35 +0545

    Modified the comments and README.md file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# Developed by Piyusha Gurung, 77261101
+
+## Tip Calculator Application
+
+It is a tip calculator application for Android where user can calculate the tip amount based on the cost of a service and the selected tip percentage and also can choose to round up the tip amount.
+
+## Key Features of Tip Calculator Application :
+
+1. XML Layout / UI
+2. Cost of Service Input
+3. Tip Calculation
+4. Tip Percentage Selection
+5. Round Up Tip
+6. Display Tip Amount
+
 ![](TipCalculator.gif)
--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -11,13 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** saves the reference to the TextView, Button, RadioGroup and Switch
+         * object in a variable called costOfServiceText, calcButton, radioGroup, tipAmountText
+         * respectively and finds them in the layout with its unique resource ID
+         */
         val costOfServiceText: TextView = findViewById<TextView>(R.id.cost_of_service)
         val calcButton: Button = findViewById<Button>(R.id.calculate_button)
         val radioGroup: RadioGroup = findViewById<RadioGroup>(R.id.tip_options)
         val roundUpSwitch: Switch = findViewById<Switch>(R.id.round_up_switch)
         val tipAmountText: TextView = findViewById<TextView>(R.id.tip_result)
 
+        //set a click listener on calcButton object by calling the setOnClickListener() method
         calcButton.setOnClickListener {
+
+            /** retrieves the cost of a service from a text field, converts it to an integer, and
+             * determines the tip percentage based on the selected radio button. The tip percentage
+             * is assigned based on the ID of the selected radio button using a when expression */
             val cost = costOfServiceText.text.toString()
             val costAmount = cost.toInt()
             val checkRadioButton: Int = radioGroup.checkedRadioButtonId
@@ -28,6 +37,9 @@ class MainActivity : AppCompatActivity() {
                 else -> 0.0
             }
 
+            /**  tip amount is calculated based on the costAmount and tipPercent
+             * If the roundUpSwitch is checked, round the tipAmount to the nearest integer and convert
+             * back to a double. Then, set the text of the tipAmountText view to display the final tip amount */
             var tipAmount = costAmount * tipPercent
             if (roundUpSwitch.isChecked) {
                 tipAmount = tipAmount.roundToInt().toDouble()
